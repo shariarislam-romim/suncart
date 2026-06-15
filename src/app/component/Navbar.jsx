16050fa546ef744847775ab73137d950d559f58c
shariarislam-romim <shariarislam46763@gmail.com>
2026-06-15 21:38:47 +0600

    set up connection

---
+++ src/app/component/Navbar.jsx
@@ -0,0 +1,43 @@
+"use client";
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <div className="border-b px-2">
+      <nav className=" flex justify-between items-center  py-3 max-w-7xl mx-auto w-full">
+        <div className="flex ">
+          
+          <h2 className="font-black text-lg">SunCart</h2>
+        </div>
+
+        <ul className="flex items-center gap-5 text-sm">
+          <li>
+            <Link href={"/"}>Home</Link>
+          </li>
+          <li>
+            <Link href={"/all-photos"}>All Products</Link>
+          </li>
+          <li>
+            <Link href={"/pricing"}>Pricing</Link>
+          </li>
+          <li>
+            <Link href={"/profile"}>Profile</Link>
+          </li>
+        </ul>
+
+        <div className="flex gap-4">
+          <ul className="flex items-center  text-sm gap-4">
+            <li>
+              <Link href={"/signup"}>SignUp</Link>
+            </li>
+            <li>
+              <Link href={"/signin"}>SignIn</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,8 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Outfit } from "next/font/google";
 import "./globals.css";
+import Navbar from "./component/Navbar";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const OutfitFont = Outfit({
   subsets: ["latin"],
 });
 
@@ -20,9 +15,12 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${OutfitFont.className}  h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <Navbar/>
+        {children}
+        </body>
     </html>
   );
 }
