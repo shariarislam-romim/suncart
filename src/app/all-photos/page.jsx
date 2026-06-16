9c2abb5d2c34c089dc7a405aa4ad5ad3ed8753ef
shariarislam-romim <shariarislam46763@gmail.com>
2026-06-16 23:45:25 +0600

    added category filter

--- src/app/all-photos/page.jsx
+++ src/app/all-photos/page.jsx
@@ -1,19 +1,34 @@
 import React from 'react';
 import PhotoCard from '../component/PhotoCard';
+import FilterPage from '../component/Filter';
 
-const AllProducts = async () => {
-        const res = await fetch('https://suncart-delta.vercel.app/data.json',{cache:"no-store"})
+const AllProducts = async ({searchParams}) => {
+
+    const params = await searchParams;
+  const category = params.category;
+    console.log(category)
+
+        const res = await fetch('http://localhost:3001/data.json',{cache:"no-store"})
     const photos = await res.json()
     // console.log(photos)
+
+     const filteredProduct = category
+    ? photos.filter(
+        (photo) =>
+          photo.category.toLowerCase() === category.toLowerCase()
+      )
+    : photos;
 
     return (
         <div>
             <h1 className="text-4xl font-bold m-4">
                 All Products</h1>
 
+            <FilterPage/>
+
             <div className="grid grid-cols-3 gap-5">
                 {
-                    photos.map(photo => <PhotoCard key={photo.id} photo={photo}>
+                    filteredProduct.map(photo => <PhotoCard key={photo.id} photo={photo}>
 
                     </PhotoCard>)
                 }
---
+++ src/app/component/Filter.jsx
@@ -0,0 +1,19 @@
+import { Button } from '@heroui/react';
+import Link from 'next/link';
+import React from 'react';
+
+const FilterPage = async () => {
+    const res = await fetch('http://localhost:3001/category.json')
+    const categories = await res.json()
+    console.log(categories)
+    return (
+        <div className='mb-5 space-x-3'>
+            {
+                categories.map(category => <Link key={category.id} href={`?category=${category.slug}`}>
+                <Button size='sm' variant='outline'>{category.name}</Button></Link>)
+            }
+        </div>
+    );
+};
+
+export default FilterPage;
